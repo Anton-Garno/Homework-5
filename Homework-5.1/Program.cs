8ff799145c0e71bb73577900b8e6fefb923ab846
Anton-Garno <garnogivo@gmail.com>
2025-06-15 19:38:48 +0200

    ok, this time all will work:) ver 1.0

--- Homework-5.1/Program.cs
+++ Homework-5.1/Program.cs
@@ -20,19 +20,23 @@ namespace SoloLearn
             {
                 Console.WriteLine("\nPlease input your first integer number:");
                 int firstNumber = Convert.ToInt32(Console.ReadLine());
+                Thread.Sleep(900);
                 Console.WriteLine($"\nFirst number is:  {firstNumber};");
                 //first block
 
                 Console.WriteLine("Please input your second number:");
                 int secondNumber = Convert.ToInt32(Console.ReadLine());
+                Thread.Sleep(900);
                 Console.WriteLine($"\nSecond number is:  {secondNumber};");
                 result = firstNumber / secondNumber;
+                Thread.Sleep(1000);
                 Console.WriteLine($"Your result is: {result}");
                 return;
             }
             catch (DivideByZeroException)
             {
                 Console.WriteLine("Opsss, you just tried division by zero... This is not good!");
+                Thread.Sleep(900);
             }
             catch(FormatException)
             {
@@ -45,17 +49,19 @@ namespace SoloLearn
             "\nAnd than you might learn the secreat of division by Zero.." +
             "\nOr you will just Die.");
             //finaly go to new stage of game
+            Thread.Sleep(900);
             Console.WriteLine("------------------");
             Thread.Sleep(2000);//pause
 
             Player player = new Player();
+            Thread.Sleep(900);
             Console.WriteLine("Enter your name Hero!");
             try//check for player name
             {
                 player.Name = Console.ReadLine(); ;//set a player Name
                 if (string.IsNullOrWhiteSpace(player.Name))
                     throw new NullOrSpaceExeption("entered nothing or space");
-
+                Thread.Sleep(900);
                 Console.WriteLine($"Our new HERO is:{player.Name}.\nWelcome to Cave Journey!!!");//get name
             }
             catch (NullOrSpaceExeption)
@@ -105,40 +111,49 @@ namespace SoloLearn
         }
         public void StartBattle()
         {
+            Thread.Sleep(500);
             Console.WriteLine("The Rules:\n1.You have only 2 HP!\n2.Dragon Has 3 HP.\nYou must give a right answer on 3 math problems.\nAnd you have only 10! Seconds\nGood luck!");
+            Thread.Sleep(500);
             Console.WriteLine($"The Battle between Dragon Zerogvard and {player.Name} has been began!");
-
+            Thread.Sleep(500);
             Console.WriteLine("Are you Ready?!(y/n)");
+            Thread.Sleep(200);
             string readyYesNo = Console.ReadLine();
-            if (readyYesNo != "y") return;
-            Thread.Sleep(3000);//epic pause
+            if (readyYesNo != "y")
+            {
+                Console.WriteLine("No? Try again later...:(");
+                return;
+            }
+            Thread.Sleep(2000);//epic pause
 
             short round = 1;
             Random random = new Random();
 
             while (dragon.IsDragonAlive && player.HealsPoints > 0)//check for heals >0
             {
-                Console.WriteLine($"=====Round{round} began!=====");
+                Console.WriteLine($"=====Round{round} began!====="); 
 
                 int firstRandNumber = random.Next(1, 20);
                 int secondRandNumber = random.Next(1, 20);
                 int mathResult = firstRandNumber + secondRandNumber;
-                Console.WriteLine(mathResult);
-                Console.WriteLine($"1 Challenge:  {firstRandNumber} + {secondRandNumber} =?!");
+                Console.WriteLine($"Challenge:  {firstRandNumber} + {secondRandNumber} =?!");
 
                 string playerAnswer = TimeOutReadLine(10000);// timeout method for answer with 10 sec// number from player
                 bool isAnswerPlayerRight = int.TryParse(playerAnswer, out int playerResult);
                  if (isAnswerPlayerRight && playerResult == mathResult)
                     {
                     dragon.HealsPoints--;
+                    Thread.Sleep(900);
                     Console.WriteLine($"Dragon has been damaged for 1HP.\n He has {dragon.HealsPoints}.");
                     if (dragon.HealsPoints == 1)
                     {
+                        Thread.Sleep(900);
                         Console.WriteLine($"{dragon.DragonName} has the las one HP. Next atack will be last..\nCan you beat him?!");
                         int lastQuestFirstNumber = 896;
                         int lastQuestSecondNumber = 789;
-                        int resultLastMathQuest = lastQuestFirstNumber + lastQuestSecondNumber;
+                        int resultLastMathQuest = lastQuestFirstNumber + lastQuestSecondNumber;//ancient question
 
+                        Thread.Sleep(900);
                         Console.WriteLine($"Solve this: {lastQuestFirstNumber} + {lastQuestSecondNumber} =!?");
                         string lastMathResult = TimeOutReadLine(8000); ;
                         int fakeResult = 1685;
@@ -147,17 +162,25 @@ namespace SoloLearn
                         {
                             throw new RightPlayerAnswerException();
                         }
+                        else
+                        {
+                            Thread.Sleep(900);
+                            Console.WriteLine("You lose and lost a chance to undestenging secrests of NULL\nTry again!");
+                            return;
+                        }
                     }
                 }
                 else
                 {
                     player.HealsPoints--;
+                    Thread.Sleep(500);
                     Console.WriteLine($"You have recive -1 HP. \nYou Have:{player.HealsPoints} HP.");
                 }
                 round++;
                 Thread.Sleep(2000);
                 if (player.HealsPoints == 0)
                 {
+                    Thread.Sleep(900);
                     Console.WriteLine("You Lose, Try again!");
                     return ;
                 }
@@ -167,7 +190,7 @@ namespace SoloLearn
         {
             string playerAnswer = null;
             Thread inputThread = new Thread(() => {
-                string playerAnswer = Console.ReadLine(); 
+            playerAnswer = Console.ReadLine(); 
             });
             inputThread.Start();
             bool completed = inputThread.Join(timeoutMilliseconds);
