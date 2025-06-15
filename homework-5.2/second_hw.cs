c5a806ae6c646e652285c9b58cc28880fcbf7e36
Anton-Garno <garnogivo@gmail.com>
2025-06-15 20:35:39 +0200

    added second homework

---
+++ homework-5.2/second_hw.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class BaseProgram
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your number:");
+
+        try
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number is: " + number);
+        }
+        catch
+        {
+            Console.WriteLine("Exception, not a number.");
+        }
+    }
+}
